b350f916375f73662fabaf4543f74b983889519c
ASUS <marina.nabil.399041@gmail.com>
2023-02-18 18:54:00 -0800

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:untitled/screens/vestimate_page.dart';
+import 'package:untitled/src/app_root.dart';
+
+void main(){
+runApp(AppRoot());
+
+}
+
